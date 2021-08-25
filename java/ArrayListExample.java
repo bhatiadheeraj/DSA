58de2d8189f71e07f8e9ec4e3ed6762f16392c23
Dheeraj <dheerajb@buffalo.edu>
2021-08-24 21:10:51 -0400

    added items to arrayList

---
+++ java/ArrayListExample.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+public class ArrayListExample {
+    public static void main(String[] args) {
+        ArrayList<Integer> arrayList = new ArrayList<>(10);
+        arrayList.add(10);
+        arrayList.add(50);
+
+        System.out.println(arrayList);
+    }
+}
