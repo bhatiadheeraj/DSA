ae7d520645d9ef7f442b73f2a6bc907ff34606ad
Dheeraj <dheerajb@buffalo.edu>
2021-09-02 16:36:39 -0400

    Implemented Stack in java

---
+++ java/Main.java
@@ -0,0 +1,13 @@
+
+public class Main {
+    public static void main(String[] args) {
+        Stack st = new Stack();
+        st.push(1);
+        st.push(40);
+        st.pop();
+        st.push(70);
+        st.push(90);
+        st.top();
+        st.print();
+    }
+}
---
+++ java/Stack.java
@@ -0,0 +1,49 @@
+import java.util.Arrays;
+
+public class Stack {
+    public static int MAX = 10000;
+    public int top;
+    int array[] = new int[MAX];
+    public Stack() {
+        /* constructor get called when object is initliazed*/
+        top = -1;
+    }
+
+    public boolean isEmpty() {
+        return (top < 0); /* automatically gets resolved*/
+    }
+
+    boolean push(int x) {
+        if(top >= (MAX - 1)) {
+            /* when top is increased such that it gets over the MAX size*/
+            return false;
+        } else {
+            top++;
+            array[top] = x;
+            return true;
+        }
+    }
+
+    int top() {
+        if(top >= 0) return array[top];
+        System.out.println("Stack underflow");
+        return 0;
+    }
+    int pop(){
+        if(top >= 0) {
+            int result = array[top];
+            top = top - 1; /* or we can do top--*/
+            return result;
+        } else {
+            System.out.println("Stack underflow");
+            return 0;
+        }
+    }
+
+    void print() {
+        for(int i = 0; i <= top; i++){
+            System.out.println(""+array[i]);
+        }
+    }
+
+}
