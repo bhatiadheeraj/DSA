b99f19346e0af0b8701bb103825a10af0d277538
Dheeraj <dheerajb@buffalo.edu>
2021-09-06 02:04:37 -0400

    added queue

--- java/Queue.java
+++ java/Queue.java
@@ -1,5 +1,41 @@
 public class Queue {
-    void Queue() {
-
+    int arraySize = 1000;
+    int[] arrayholder;
+    int length_checker = 0;
+    Queue() {
+         arrayholder = new int[arraySize];
     }
+    public void enqueue (int input){
+        if (length_checker+1 <= arraySize) {
+            arrayholder[length_checker] = input;
+            length_checker++;
+            return;
+        }else{
+            int[] new_array = new int[arraySize*2];
+            for(int i=0; i <arraySize;i++) {
+                new_array[i] = arrayholder[i];
+            }
+            arrayholder = new_array;
+            arraySize = arraySize *2;
+        }
+    }
+    public void dequeue() {
+        arrayholder[0] = 0;
+        for(int i=1; i<=length_checker;i++) {
+            arrayholder[i-1] = arrayholder[i];
+        }
+        arrayholder[length_checker] = 0;
+        length_checker--;
+        for(int i=0; i<length_checker;i++) {
+            System.out.println(""+arrayholder[i]);
+        }
+    }
+    public int front () {
+     return arrayholder[0];
+    }
+
+    public int rear() {
+        return arrayholder[length_checker-1];
+    }
+
 }
